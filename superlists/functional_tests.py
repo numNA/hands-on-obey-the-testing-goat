c13503014ddbe8bd3330976671ddbbaaff56d4bc
numNA <numna1018@gmail.com>
2023-08-12 14:57:16 +0900

    Refactor test codes

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,7 +10,12 @@ class NewVisitorTest(unittest.TestCase):
     
     def tesrDown(self):
         self.browser.quit()
-
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_strat_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do add. 
         # She goes to check out its home page.
@@ -33,18 +38,21 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        time.sleep(3)
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
+         #self.fail('Finish the test!')
+         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
         # She visits that URL - her to-do list is still there.
         # Satisfied, she goes back to sleep
