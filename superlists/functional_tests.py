478a618a785f28e6eaf7ea9e77dee8a51106a66b
numNA <numna1018@gmail.com>
2023-08-12 17:33:23 +0900

    Push all

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
     
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
     
     def test_can_strat_a_list_and_retrieve_it_later(self):
@@ -49,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
+        self.fail('Finish the test!')
         # Edith wonders whether the site will remember her list. Then she sees
          #self.fail('Finish the test!')
          # that the site has generated a unique URL for her -- there is some
